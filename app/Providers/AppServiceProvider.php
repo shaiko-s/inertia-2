4c13e13ad45d0441761bf743de18f9980e746378
shaiko-s <145833407+shaiko-s@users.noreply.github.com>
2024-10-20 16:18:16 +0300

    Update AppServiceProvider to register morph maps for ingredient, semiproduct, and finalproduct models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        Relation::morphMap([
+            'ingredient' => 'App\Models\Ingredient',
+            'semiproduct' => 'App\Models\Semiproduct',
+            'finalproduct' => 'App\Models\Finalproduct',
+        ]);
     }
 }
